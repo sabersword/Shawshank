15571834fd312a707315629685aeff8185d74e09
sabersword <godigmh@gmail.com>
2018-10-25 20:54:04 +0800

    清理rebase测试代码

--- product-client/src/main/java/org/ypq/ProductClientApplication.java
+++ product-client/src/main/java/org/ypq/ProductClientApplication.java
@@ -10,19 +10,6 @@ public class ProductClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductClientApplication.class, args);
-        System.out.println("bug commit 1  C3");
-        System.out.println("bug commit 2  C4");
-        System.out.println("bug commit 3  C10");
-    }
-
-    public void test() {
-        System.out.println("master commit 1 C5");
-        System.out.println("master commit 2 C6");
-    }
-
-    public void hotfix() {
-        System.out.println("hotfix commit  1  C8");
-        System.out.println("hotfix commit  2  C9");
     }
 }
 
