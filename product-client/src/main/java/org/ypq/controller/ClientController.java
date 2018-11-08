527246cb10104488deabb60b8535a9c188f3f77d
sabersword <godigmh@gmail.com>
2018-11-08 19:00:53 +0800

    client也部署

--- product-client/src/main/java/org/ypq/controller/ClientController.java
+++ product-client/src/main/java/org/ypq/controller/ClientController.java
@@ -1,29 +1,23 @@
 package org.ypq.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.ypq.api.ProductAPI;
 import org.ypq.domain.Product;
 import org.ypq.service.ProductService;
 
 import java.util.List;
 
-@CacheConfig(cacheNames = {"ypq.cache"})
 @RestController
-public class ProductController implements ProductAPI {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+public class ClientController {
 
     @Autowired
     private ProductService productService;
 
-    @Override
+    @RequestMapping(value = "/testGetOneProduct")
     public Product getOneProduct(int id) {
         Product product = new Product();
         product.setId(11);
@@ -31,23 +25,19 @@ public class ProductController implements ProductAPI {
         return product;
     }
 
-    @Override
+    @RequestMapping(value = "/testGetProducts")
     public List<Product> getProducts() {
-        return productService.generateProducts();
+        return productService.getProducts();
     }
 
-    @Cacheable(key = "'productCache' + #id")
     @RequestMapping(value = "/get")
     public Product get(int id) {
-        LOGGER.info("对id={}进行了缓存", id);
         // TODO 数据库耗时操作
         return new Product(id, "我要测试缓存");
     }
 
-    @CacheEvict(key = "'productCache' + #id")
     @RequestMapping(value = "/delete")
     public void delete(int id) {
-        LOGGER.info("删除了id={}的缓存", id);
     }
 
 }
--- product-service/src/main/java/org/ypq/ProductServiceApplication.java
+++ product-service/src/main/java/org/ypq/ProductServiceApplication.java
@@ -1,47 +1,18 @@
 package org.ypq;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableCaching
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
 
-//    @Bean
-//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        // 1.创建 redisTemplate 模版
-//        RedisTemplate<Object, Object> template = new RedisTemplate<>();
-//        // 2.关联 redisConnectionFactory
-//        template.setConnectionFactory(redisConnectionFactory);
-//        // 3.创建 序列化类
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-//        ObjectMapper om = new ObjectMapper();
-//        // 4.设置可见度
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        // 5.启动默认的类型
-//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        // 6.序列化类，对象映射设置
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
-//        // 7.设置 value 的转化格式和 key 的转化格式
-//        template.setValueSerializer(jackson2JsonRedisSerializer);
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.afterPropertiesSet();
-//        return template;
-//    }
 
 }
