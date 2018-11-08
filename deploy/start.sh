57983ea533c00efbb24238c54016029760bd5a9f
sabersword <godigmh@gmail.com>
2018-11-08 22:08:28 +0800

    修改Linux脚本风格为xxx-xxx

--- deploy/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]
-then
-  echo "参数不符合,请带上profile,如server1,server2"
-else
-  echo "得到参数$1,启动程序"
-  nohup java -jar eureka-server-*.jar --spring.profiles.active="$1" > "$1".log 2>&1 &
-fi
