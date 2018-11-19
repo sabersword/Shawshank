1b6400ca9e1345abc5279c6db2c744b84fe8ebfe
sabersword <godigmh@gmail.com>
2018-11-19 21:15:03 +0800

    本地docker构建的时候把之前的镜像删了

--- jenkins/build-shell.sh
+++ jenkins/build-shell.sh
@@ -6,4 +6,5 @@ mvn clean package -Dmaven.test.skip=true
 cd product-service
 mvn docker:build
 docker ps | grep '8080'|awk '{print $1}'|xargs -r docker rm -f
+docker images | grep none | awk '{print $3}' | xargs -rt docker rmi -f
 docker run -v /root/dockerlog -d -p 8080:8080 --add-host=docker-host:`docker network inspect  --format='{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge` springboot/product-service
