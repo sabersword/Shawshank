351e92bbf9c1689b5e86786f6b5d4410394d0d98
sabersword <godigmh@gmail.com>
2018-12-08 15:56:40 +0800

    示例docker没用了,删掉

--- docker/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker ps | grep '8080'|awk '{print $1}'|xargs -r docker rm -f
-docker run -v /root/dockerlog -d -p 8080:8080 --add-host=docker-host:`docker network inspect  --format='{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge` springboot/product-service
